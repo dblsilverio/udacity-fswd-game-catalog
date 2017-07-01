01e1080d1cddff4e12de47f20f3d77a6bedea603
Diogo Silvério <dblsilverio@gmail.com>
2017-07-01 00:34:17 -0300

    Encoding and profile picture folder.

--- catalog/__init__.py
+++ catalog/__init__.py
@@ -1,5 +1,5 @@
 """ Game Catalog project for Full Stack Web Developer. """
-
+import sys
 from catalog.infra.db_factory import create
 from catalog.infra.flask_factory import app
 from catalog.infra.bootstrap import Bootstrap
@@ -7,6 +7,9 @@ import catalog.routes.main
 import catalog.routes.category
 import catalog.routes.game
 import catalog.routes.access
+
+reload(sys)
+sys.setdefaultencoding('utf8')
 
 create()
 Bootstrap.create_categories()
